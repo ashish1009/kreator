30506781f3ac014952d2d6be8848a6bbd881cc33
Ashish <aashishkan1009@gmail.com>
2022-12-25 19:23:28 +0530

    20. Logging Profiler results in debug mode on terminal

--- ikan/implementation/core/debug/profiler.cpp
+++ ikan/implementation/core/debug/profiler.cpp
@@ -17,11 +17,19 @@ namespace ikan {
     end_tIme_point_ = std::chrono::high_resolution_clock::now();;
     duration_ = end_tIme_point_ - start_time_point_;
     
+#ifdef IK_DEBUG_FEATURE
+    IK_TRACE("  Execution time for ");
+    IK_TRACE("  | Function      | {0}", function_name_);
+    IK_TRACE("  | seconds       | {0}", duration_.count());
+    IK_TRACE("  | mili seconds  | {0}", duration_.count() * 1000);
+    IK_TRACE("  | micro seconds | {0}", duration_.count() * 1000000);
+#else
     std::cout << "  Execution time for \n";
     std::cout << "  | Function      | " << function_name_ << "\n";
     std::cout << "  | seconds       | " << duration_.count() << "\n";
     std::cout << "  | mili seconds  | " << duration_.count() * 1000 << "\n";
     std::cout << "  | micro seconds | " << duration_.count() * 1000000 << "\n";
+#endif
   }
   
 }
