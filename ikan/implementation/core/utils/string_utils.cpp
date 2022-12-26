804f202d21f31f691ab574a263c1afbcad116345
Ashish <aashishkan1009@gmail.com>
2022-12-26 13:54:53 +0530

    41. Adding APIs in string Utils class to parse the strings

--- ikan/implementation/core/utils/string_utils.cpp
+++ ikan/implementation/core/utils/string_utils.cpp
@@ -38,6 +38,16 @@ namespace ikan {
     return path.substr(last_dot, count);
   }
   
+  /// This function returns the file name from path as string
+  ///         0           last_slash
+  ///         |              |
+  /// e.g. : "folder1/folder2/file_name.extention"
+  ///         return "file_name"
+  /// - Parameter path: path of file
+  std::string StringUtils::GetDirectoryFromFilePath(const std::string& path) {
+    return path.substr(0, path.find_last_of('/'));
+  }
+  
   std::string StringUtils::ReadFromFile(const std::string& file_path) {
     std::string result = "";
     std::ifstream in(file_path, std::ios::in | std::ios::binary);
@@ -60,5 +70,95 @@ namespace ikan {
     return result;
   }
 
+  const char* StringUtils::FindToken(const char* str, const std::string& token) {
+    const char* t = str;
+    while ((t = strstr(t, token.c_str()))) {
+      // Check if left character of token is empty
+      // using '/' as indicator of first character of line
+      bool left = str == t or isspace(t[-1]) or t[-1] == '/';
+      
+      // Check if right character of token is empty
+      bool right = !t[token.size()] or isspace(t[token.size()]);
+      
+      // Checking token is whole word
+      if (left and right)
+        return t;
+      
+      t += token.size();
+    }
+    return nullptr;
+  }
   
+  std::string StringUtils::GetBlock(const char* str, const char** out_position) {
+    // get the end block brace of the string code
+    const char* end = strstr(str, "}");
+    
+    // if not found any end bkock statement "}" then return the same string
+    if (!end)
+      return str;
+    
+    // Update the out_position pointer after the end block
+    // offset the shader string to the end of current strcut
+    if (out_position)
+      *out_position = end;
+    
+    uint32_t length = (uint32_t)(end - str + 1);
+    // return the substring from start to last "}"
+    return std::string(str, length);
+  }
+  
+  std::string StringUtils::GetStatement(const char* str, const char** outPosition) {
+    // get the end block brace of the string code
+    const char* end = strstr(str, ";");
+    
+    // if not found any end bkock statement ";" then return the same string
+    if (!end)
+      return str;
+    
+    // Update the out_position pointer after the end block
+    // offset the shader string to the end of current strcut
+    if (outPosition)
+      *outPosition = end;
+    
+    uint32_t length = (uint32_t)(end - str + 1);
+    // return the substring from start to last ";"
+    return std::string(str, length);
+  }
+  
+  std::vector<std::string> StringUtils::SplitString(const std::string& string, const std::string& delimiters) {
+    size_t start = 0;
+    // Find the first occurance of splitter
+    size_t end = string.find_first_of(delimiters);
+    
+    // Stores the result
+    std::vector<std::string> result;
+    
+    // while end of string
+    while (end <= std::string::npos) {
+      // Store the split block
+      std::string token = string.substr(start, end - start);
+      if (!token.empty())
+        result.push_back(token);
+      
+      if (end == std::string::npos)
+        break;
+      
+      start = end + 1;
+      end = string.find_first_of(delimiters, start);
+    }
+    return result;
+  }
+  
+  std::vector<std::string> StringUtils::SplitString(const std::string& string, const char delimiter) {
+    return SplitString(string, std::string(1, delimiter));
+  }
+  
+  std::vector<std::string> StringUtils::Tokenize(const std::string& string) {
+    return SplitString(string, " \t\n");
+  }
+  
+  std::vector<std::string> StringUtils::GetLines(const std::string& string) {
+    return SplitString(string, "\n");
+  }
+
 }
--- ikan/interface/core/utils/string_utils.hpp
+++ ikan/interface/core/utils/string_utils.hpp
@@ -23,12 +23,58 @@ namespace ikan {
     ///         return "extension"
     /// - Parameter path: file path for which file Extenstion need to be extracted
     static std::string GetExtensionFromFilePath(const std::string& path);
-    
+    /// This function extracts the file directory from file path
+    /// e.g. : "folder1/folder2/file_name.extention"
+    ///         return "folder1/folder2/file_name"
+    /// - Parameter path: file path for which file Extenstion need to be extracted
+    static std::string GetDirectoryFromFilePath(const std::string& path);
     /// This function read the file from path given in 'file_path' and store
     /// all the date in the file in a string. Return the content of this give
     /// file as string
     /// - Parameter path: file path to be read
     static std::string ReadFromFile(const std::string& file_path);
+    
+    /// This function finds a word/token from string and return the char pointer
+    /// starting the token
+    /// - Parameters:
+    ///   - str: base tring from which token need to be searched
+    ///   - token: toke to be searched
+    static const char*FindToken(const char* str, const std::string& token);
+    /// This function finds a block of string from string and return the char
+    /// pointer starting the strong 'str'
+    /// - Parameters:
+    ///   - str: base tring from which block need to be searched
+    ///   - out_position: position of start of block as char pointer
+    static std::string GetBlock(const char* str, const char** out_position);
+    
+    /// This function extracts the Code statement before ";"
+    /// - Parameters:
+    ///   - str: token to be extracted from outPosition
+    ///   - outPosition: string pointer of the shader code, that will move to
+    /// some offset offset is the ";" of the strcut
+    static std::string GetStatement(const char* str, const char** outPosition);
+    /// This function breaks the string. Break points are stored in delimiter,
+    /// any character in the delimeter will be cosnider as break points
+    /// - Parameters:
+    ///   - string: broken strings vector
+    ///   - delimiters: (break point of string). From here string will be split
+    static std::vector<std::string> SplitString(const std::string& string, const std::string& delimiters);
+    /// This function breaks the string. Break points are stored in delimiter,
+    /// any character in the delimeter will be cosnider as break points
+    /// - Parameters:
+    ///   - string: broken strings vector
+    ///   - delimiters: (break point of char). From here string will be split
+    static std::vector<std::string> SplitString(const std::string& string, const char delimiter);
+    /// This function breaks the string. Break points are stored in delimiter,
+    /// any character in the delimeter will be cosnider as break points
+    /// Split point (\t\n).
+    /// - Parameter string: broken strings vector
+    static std::vector<std::string> Tokenize(const std::string& string);
+    /// This function breaks the string. Break points are stored in delimiter,
+    /// any character in the delimeter will be cosnider as break points
+    /// Split point (\n).
+    /// - Parameter string: broken strings vector
+    static std::vector<std::string> GetLines(const std::string& string);
   };
   
 }
