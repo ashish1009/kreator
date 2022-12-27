6d86b51c9aad06b12741ba6491655c4f1a390fa7
Ashish <aashishkan1009@gmail.com>
2022-12-28 01:14:16 +0530

    98. Adding API to Load the font for text Rendering

--- ikan/implementation/renderer/utils/text_renderer.cpp
+++ ikan/implementation/renderer/utils/text_renderer.cpp
@@ -109,4 +109,41 @@ namespace ikan {
     delete text_data_;
   }
   
+  void TextRenderer::LoadFreetype(const std::string& font_file_path) {
+    IK_CORE_DEBUG("Loading the Font for Text renderer | '{0}'", font_file_path.c_str());
+    
+    FT_Library ft;
+    IK_CORE_ASSERT(!FT_Init_FreeType(&ft), "Not able to load Freetype");
+    
+    // find path to font
+    IK_CORE_ASSERT(!font_file_path.empty(), "Not able to load Font");
+    
+    // load font as face
+    FT_Face face;
+    IK_CORE_ASSERT(!FT_New_Face(ft, font_file_path.c_str(), 0, &face),
+                   "Unavle to load tha font to freetype");
+    // set size to load glyphs as
+    FT_Set_Pixel_Sizes(face, 0, 48);
+    
+    // load first 128 characters of ASCII set
+    for (unsigned char c = 0; c < 128; c++) {
+      // Load character glyph
+      if (FT_Load_Char(face, c, FT_LOAD_RENDER)){
+        IK_CORE_ERROR("ERROR::FREETYTPE: Failed to load Glyph");
+        continue;
+      }
+      // now store character for later use
+      std::shared_ptr<CharTexture> char_texture = CharTexture::Create(face,
+                                                                      glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+                                                                      glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+                                                                      static_cast<uint32_t>(face->glyph->advance.x),
+                                                                      char(c));
+      text_data_->char_texture_map.insert(std::pair<char, std::shared_ptr<CharTexture>>(c, char_texture));
+    }
+    
+    // destroy FreeType once we're finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+  }
+  
 }
--- ikan/interface/renderer/utils/text_renderer.hpp
+++ ikan/interface/renderer/utils/text_renderer.hpp
@@ -21,6 +21,11 @@ namespace ikan {
     static void Init();
     /// This function Shuts down on Text the Batch Renderer. Delete all the allocated Data
     static void Shutdown();
+    
+    /// This function loads the font for freetype
+    /// - Parameter font_file_path: path of font
+    /// NOTE: Load the Font before using any other API In clientw
+    static void LoadFreetype(const std::string& font_file_path);
   };
   
 }
