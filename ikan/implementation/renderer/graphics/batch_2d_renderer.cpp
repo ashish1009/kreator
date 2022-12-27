57c9154d86c86d26dcefb0ed52b2801f70c22757
Ashish <aashishkan1009@gmail.com>
2022-12-27 22:04:55 +0530

    74. Creating Batch Data structure for Quad

--- ikan/implementation/renderer/graphics/batch_2d_renderer.cpp
+++ ikan/implementation/renderer/graphics/batch_2d_renderer.cpp
@@ -34,6 +34,10 @@ namespace ikan {
 
   /// This structure holds the common batch renderer data for Quads and circle
   struct BatchRendererData {
+    // Constants
+    static constexpr uint32_t VertexForSingleElement = 4;
+    static constexpr uint32_t IndicesForSingleElement = 6;
+
     /// This structure stores the environmant data
     struct Environment {
       glm::mat4 camera_view_projection_matrix;
@@ -62,17 +66,63 @@ namespace ikan {
     /// Texture Slot index sent to Shader to render a specific Texture from slots
     /// Slot 0 is reserved for white texture (No Image only color)
     uint32_t texture_slot_index = 1; // 0 = white texture
-    
+
+    /// Max element to be rendered in single batch
+    uint32_t max_element = 0;
+
     /// Basic vertex of quad
     /// Vertex of circle is taken as Quad only
     glm::vec4 vertex_base_position[4];
+        
+    void StartCommonBatch() {
+      index_count = 0;
+      texture_slot_index = 1;
+    }
     
     /// Virtual Destructor
-    virtual ~BatchRendererData() = default;
+    virtual ~BatchRendererData() {
+      RendererStatistics::Get().index_buffer_size -= max_indices * sizeof(uint32_t);
+    }
+
+    friend class QuadData;
+
   private:
     BatchRendererData() = default;
   };
-
+  
+  /// Batch Data to Rendering Quads
+  struct QuadData : BatchRendererData {
+    /// Single vertex of a Quad
+    struct Vertex : BaseVertex {
+      int32_t object_id;        // Pixel ID of Quad
+    };
+    
+    /// Base pointer of Vertex Data. This is start of Batch data for single draw call
+    Vertex* vertex_buffer_base_ptr = nullptr;
+    /// Incrememntal Vetrtex Data Pointer to store all the batch data in Buffer
+    Vertex* vertex_buffer_ptr = nullptr;
+    
+    /// Constructor
+    QuadData() {
+      IK_CORE_TRACE("Creating QuadData instance ...");
+    }
+    /// Destructor
+    virtual ~QuadData() {
+      IK_CORE_WARN("Destroying QuadData instance and clearing the data !!!");
+      delete [] vertex_buffer_base_ptr;
+      vertex_buffer_base_ptr = nullptr;
+      
+      RendererStatistics::Get().vertex_buffer_size -= max_vertices * sizeof(QuadData::Vertex);
+    }
+    
+    /// start new batch for quad rendering
+    void StartBatch() {
+      StartCommonBatch();
+      vertex_buffer_ptr = vertex_buffer_base_ptr;
+    }
+  };
+  static QuadData* quad_data_;
+  
   // --------------------------------------------------------------------------
   // Batch Renderer API
   // --------------------------------------------------------------------------
