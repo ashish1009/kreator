4c1aa79fb58fcbb5a4d20915a408a288a6b91598
Ashish <aashishkan1009@gmail.com>
2023-01-02 20:10:20 +0530

    197. Adding Vulkan
    - Adding Vulkan Library
    - Adding Vulkan Includes
    - Testing GLFW with Vulkan

--- kreator/entry_point/entry_point.cpp
+++ kreator/entry_point/entry_point.cpp
@@ -10,20 +10,29 @@
 #define Vulkan 1
 #if Vulkan
 
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 int main() {
   std::cout << "vulkan Main ... \n";
   
+  bool success =  glfwInit();
+  if (!success){
+    std::cout << "Failed \n";
+  }
   assert(GLFW_TRUE == glfwInit());
   
-  GLFWwindow* window_ = glfwCreateWindow((int32_t)1600,
-                                         (int32_t)900,
+  GLFWwindow* window_ = glfwCreateWindow((int32_t)800,
+                                         (int32_t)800,
                                          "Vulkan",
                                          nullptr, /* External Monitor Pointer : NO
                                                    Use current Selected Monitor */
                                          nullptr  /* Share Monitor : NO */
                                          );
+  
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
 
   // If Window is not created successfully then terminate...
   if (window_ == NULL) {
@@ -33,6 +42,10 @@ int main() {
   
   // Setting VSync as True
   glfwSwapInterval(1);
+  
+  auto t = glfwVulkanSupported();
+//  assert(glfwVulkanSupported());
+
   
   while (!glfwWindowShouldClose(window_)) {
     glfwPollEvents();
