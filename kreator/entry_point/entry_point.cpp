a709a0c43b44c35f77cf2e8ade59159ceff3178c
Ashish <aashishkan1009@gmail.com>
2023-01-03 15:01:35 +0530

    201. Created Logical device in vulkan

--- kreator/entry_point/entry_point.cpp
+++ kreator/entry_point/entry_point.cpp
@@ -17,6 +17,8 @@
 
 inline static VkInstance s_VulkanInstance;
 inline static VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+inline static VkDevice device;
+inline static VkQueue graphicsQueue;
 
 std::vector<const char*> getRequiredExtensions() {
   uint32_t glfwExtensionCount = 0;
@@ -167,6 +169,41 @@ int main() {
   // ------------------------
   // Create Logical Device
   // ------------------------
+  QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+  
+  VkDeviceQueueCreateInfo queueCreateInfo{};
+  queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+  queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+  queueCreateInfo.queueCount = 1;
+  
+  float queuePriority = 1.0f;
+  queueCreateInfo.pQueuePriorities = &queuePriority;
+  
+  VkPhysicalDeviceFeatures deviceFeatures{};
+  
+  VkDeviceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+  
+  createInfo.pQueueCreateInfos = &queueCreateInfo;
+  createInfo.queueCreateInfoCount = 1;
+  
+  createInfo.pEnabledFeatures = &deviceFeatures;
+  
+  createInfo.enabledExtensionCount = 0;
+  
+//  if (enableValidationLayers) {
+//    createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+//    createInfo.ppEnabledLayerNames = validationLayers.data();
+//  } else {
+    createInfo.enabledLayerCount = 0;
+//  }
+  
+  if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+
+  }
+  
+  vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+
 
   while (!glfwWindowShouldClose(window_)) {
     glfwPollEvents();
