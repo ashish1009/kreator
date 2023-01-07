8a668b17a53abc4f049401e4e7dbb124cdecb377
Ashish <aashishkan1009@gmail.com>
2023-01-07 18:12:07 +0530

    275. Scriptable Entity class and manager to manage native scripts

---
+++ ikan/implementation/scene/scriptable_entity.cpp
@@ -0,0 +1,28 @@
+//
+//  scriptable_entity.cpp
+//  ikan
+//
+//  Created by Ashish . on 07/01/23.
+//
+
+#include "scriptable_entity.hpp"
+#include "scene/core_components.hpp"
+
+namespace ikan {
+  
+  // --------------------------------------------------------------------------
+  // Script Manager
+  // --------------------------------------------------------------------------
+  std::vector<std::string> ScriptManager::scripts_ = {
+    "ikan::ScriptableEntity",
+  };
+  
+  void ScriptManager:: UpdateScript(NativeScriptComponent* sc,
+                                    const std::string& script_name) {
+    if (script_name == "editor::ScriptableEntity")
+      sc->Bind<ikan::ScriptableEntity>();
+    else
+      IK_ASSERT(false, "Invalid Script name");
+  }
+  
+}
---
+++ ikan/interface/scene/scriptable_entity.hpp
@@ -0,0 +1,56 @@
+//
+//  scriptable_entity.h
+//  ikan
+//
+//  Created by Ashish . on 07/01/23.
+//
+
+
+#pragma once
+
+#include "scene/entity.hpp"
+
+namespace ikan {
+  
+  // ---------------------------------------------------------------------------
+  // This file holds all the native scripts. For now we are using only C type
+  // Scripts so we have to pre define all of them. No Runtime script is supported
+  // yet. So store all the script name as their class name in ScriptManager::scripts_
+  // ---------------------------------------------------------------------------
+  
+  class NativeScriptComponent;
+  
+  class ScriptManager {
+  public:
+    static std::vector<std::string> scripts_;
+
+    /// This function update the scrip bind function pointer
+    /// - Parameters:
+    ///   - sc: Native script component pointer
+    ///   - script_name: Script name
+    static void UpdateScript(NativeScriptComponent* sc, const std::string& script_name);
+    
+    MAKE_PURE_STATIC(ScriptManager);
+  };
+  
+  class ScriptableEntity {
+  public:
+    virtual ~ScriptableEntity() {}
+
+    template<typename T>
+    T& GetComponent() {
+      return entity_.GetComponent<T>();
+    }
+
+  protected:
+    virtual void Create() {}
+    virtual void Destroy() {}
+    virtual void Update(Timestep ts) {}
+
+  protected:
+    Entity entity_;
+    EnttScene* scene_;
+    friend class Scene;
+  };
+  
+}
