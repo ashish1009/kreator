da064604511d9c79423a30724c6823a572b282fc
Ashish <aashishkan1009@gmail.com>
2023-01-14 14:46:38 +0530

    430. Physics: Creating Edge Class to store edge

--- ikan/interface/physics/physics_common.h
+++ ikan/interface/physics/physics_common.h
@@ -7,8 +7,13 @@
 
 #pragma once
 
+#include "settings.hpp"
+
 namespace physics {
   
 #define NOT_USED(x) ((void)(x))
   
+#define LinearSlop      (0.005f * LengthUnitsPerMeter)
+#define PolygonRadius    (2.0f * LinearSlop)
+
 }
