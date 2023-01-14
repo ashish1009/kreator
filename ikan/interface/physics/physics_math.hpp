6a1b3fbef2cdddcc906758c99c0b94418b43dabb
Ashish <aashishkan1009@gmail.com>
2023-01-14 12:44:52 +0530

    419. Physics: Adding operators in Maths

--- ikan/interface/physics/physics_math.hpp
+++ ikan/interface/physics/physics_math.hpp
@@ -325,6 +325,25 @@ namespace physics {
     return Vec2(Dot(v, A.ex), Dot(v, A.ey));
   }
   
+  /// Add two vectors component-wise.
+  inline Vec2 operator + (const Vec2& a, const Vec2& b) {
+    return Vec2(a.x + b.x, a.y + b.y);
+  }
   
-
+  /// Subtract two vectors component-wise.
+  inline Vec2 operator - (const Vec2& a, const Vec2& b) {
+    return Vec2(a.x - b.x, a.y - b.y);
+  }
+  
+  inline Vec2 operator * (float s, const Vec2& a) {
+    return Vec2(s * a.x, s * a.y);
+  }
+  
+  inline bool operator == (const Vec2& a, const Vec2& b) {
+    return a.x == b.x && a.y == b.y;
+  }
+  
+  inline bool operator != (const Vec2& a, const Vec2& b) {
+    return a.x != b.x || a.y != b.y;
+  }
 }
