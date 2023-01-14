e1b5fe1cd57b8338868f0ee46fb563ef448a9373
Ashish <aashishkan1009@gmail.com>
2023-01-14 18:53:36 +0530

    447. Physics: APIs Implementations

--- ikan/implementation/physics/collision.cpp
+++ ikan/implementation/physics/collision.cpp
@@ -10,6 +10,39 @@
 
 namespace physics {
   
+  // Sutherland-Hodgman clipping.
+  int32_t ClipSegmentToLine(ClipVertex vOut[2], const ClipVertex vIn[2],
+                            const Vec2& normal, float offset, int32_t vertexIndexA) {
+    // Start with no output points
+    int32_t count = 0;
+    
+    // Calculate the distance of end points to the line
+    float distance0 = Dot(normal, vIn[0].v) - offset;
+    float distance1 = Dot(normal, vIn[1].v) - offset;
+    
+    // If the points are behind the plane
+    if (distance0 <= 0.0f) vOut[count++] = vIn[0];
+    if (distance1 <= 0.0f) vOut[count++] = vIn[1];
+    
+    // If the points are on different sides of the plane
+    if (distance0 * distance1 < 0.0f) {
+      // Find intersection point of edge and plane
+      float interp = distance0 / (distance0 - distance1);
+      vOut[count].v = vIn[0].v + interp * (vIn[1].v - vIn[0].v);
+      
+      // VertexA is hitting edgeB.
+      vOut[count].id.cf.indexA = static_cast<uint8_t>(vertexIndexA);
+      vOut[count].id.cf.indexB = vIn[0].id.cf.indexB;
+      vOut[count].id.cf.typeA = (uint8_t)ContactFeature::Type::Vertex;
+      vOut[count].id.cf.typeB = (uint8_t)ContactFeature::Type::Face;
+      ++count;
+      
+      IK_ASSERT(count == 2);
+    }
+    
+    return count;
+  }
+  
   bool TestOverlap(  const Shape* shapeA, int32_t indexA,
                      const Shape* shapeB, int32_t indexB,
                      const Transform& xfA, const Transform& xfB) {
