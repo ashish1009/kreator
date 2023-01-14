3759327b628d072c6c5a89660c6e733105e05b78
Ashish <aashishkan1009@gmail.com>
2023-01-14 13:37:22 +0530

    423. Physics: Adding Physics AABB Class

--- ikan/interface/physics/collision.hpp
+++ ikan/interface/physics/collision.hpp
@@ -23,4 +23,55 @@ namespace physics {
     float fraction;
   };
   
+  /// An axis aligned bounding box.
+  struct AABB {
+    /// Verify that the bounds are sorted.
+    bool IsValid() const;
+    
+    /// Get the center of the AABB.
+    Vec2 GetCenter() const {
+      return 0.5f * (lowerBound + upperBound);
+    }
+    
+    /// Get the extents of the AABB (half-widths).
+    Vec2 GetExtents() const {
+      return 0.5f * (upperBound - lowerBound);
+    }
+    
+    /// Get the perimeter length
+    float GetPerimeter() const {
+      float wx = upperBound.x - lowerBound.x;
+      float wy = upperBound.y - lowerBound.y;
+      return 2.0f * (wx + wy);
+    }
+    
+    /// Combine an AABB into this one.
+    void Combine(const AABB& aabb) {
+      lowerBound = Min(lowerBound, aabb.lowerBound);
+      upperBound = Max(upperBound, aabb.upperBound);
+    }
+    
+    /// Combine two AABBs into this one.
+    void Combine(const AABB& aabb1, const AABB& aab) {
+      lowerBound = Min(aabb1.lowerBound, aab.lowerBound);
+      upperBound = Max(aabb1.upperBound, aab.upperBound);
+    }
+    
+    /// Does this aabb contain the provided AABB.
+    bool Contains(const AABB& aabb) const {
+      bool result = true;
+      result = result && lowerBound.x <= aabb.lowerBound.x;
+      result = result && lowerBound.y <= aabb.lowerBound.y;
+      result = result && aabb.upperBound.x <= upperBound.x;
+      result = result && aabb.upperBound.y <= upperBound.y;
+      return result;
+    }
+    
+    bool RayCast(RayCastOutput* output, const RayCastInput& input) const;
+    
+    Vec2 lowerBound;  ///< the lower vertex
+    Vec2 upperBound;  ///< the upper vertex
+  };
+
+  
 }
