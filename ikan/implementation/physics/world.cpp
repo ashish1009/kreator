b5e574b23476cf848b7a21faa75488117787458f
Ashish <aashishkan1009@gmail.com>
2023-01-17 11:14:36 +0530

    478. Physics: Solve API in World Class

--- ikan/implementation/physics/world.cpp
+++ ikan/implementation/physics/world.cpp
@@ -9,6 +9,9 @@
 #include "body.hpp"
 #include "fixture.hpp"
 #include "timer.hpp"
+#include "contact.hpp"
+#include "joint.hpp"
+#include "dynamics/is_land.hpp"
 
 namespace physics {
   
@@ -26,7 +29,7 @@ namespace physics {
   }
   
   World::~World() {
-    // Some shapes allocate using b2Alloc.
+    // Some shapes allocate using Alloc.
     Body* b = body_list_;
     while (b) {
       Body* bNext = b->next_;
@@ -102,6 +105,145 @@ namespace physics {
   
   // Find islands, integrate and solve constraints, solve position constraints
   void World::Solve(const TimeStep& step) {
+    profile_.solveInit = 0.0f;
+    profile_.solveVelocity = 0.0f;
+    profile_.solvePosition = 0.0f;
+    
+    // Size the island for the worst case.
+    Island island(body_count_,
+                  contact_manager_.contact_count_,
+                  joint_count_,
+                  &stack_allocator_,
+                  contact_manager_.contact_listener_);
+    
+    // Clear all the island flags.
+    for (Body* b = body_list_; b; b = b->next_) {
+      b->flags_ &= ~Body::e_islandFlag;
+    }
+    for (Contact* c = contact_manager_.contact_list_; c; c = c->next_) {
+      c->flags_ &= ~Contact::IslandFlag;
+    }
+    for (Joint* j = joint_list_; j; j = j->next_) {
+      j->is_land_flag_ = false;
+    }
+    
+    // Build and simulate all awake islands.
+    int32_t stackSize = body_count_;
+    Body** stack = (Body**)stack_allocator_.Allocate(stackSize * sizeof(Body*));
+    for (Body* seed = body_list_; seed; seed = seed->next_) {
+      if (seed->flags_ & Body::e_islandFlag) {
+        continue;
+      }
+      
+      if (seed->IsAwake() == false || seed->IsEnabled() == false) {
+        continue;
+      }
+      
+      // The seed can be dynamic or kinematic.
+      if (seed->GetType() == BodyType::StaticBody) {
+        continue;
+      }
+      
+      // Reset island and stack.
+      island.Clear();
+      int32_t stackCount = 0;
+      stack[stackCount++] = seed;
+      seed->flags_ |= Body::e_islandFlag;
+      
+      // Perform a depth first search (DFS) on the constraint graph.
+      while (stackCount > 0) {
+        // Grab the next body off the stack and add it to the island.
+        Body* b = stack[--stackCount];
+        IK_ASSERT(b->IsEnabled() == true);
+        island.Add(b);
+        
+        // To keep islands as small as possible, we don't
+        // propagate islands across static bodies.
+        if (b->GetType() == BodyType::StaticBody) {
+          continue;
+        }
+        
+        // Make sure the body is awake (without resetting sleep timer).
+        b->flags_ |= Body::e_awakeFlag;
+        
+        // Search all contacts connected to this body.
+        for (ContactEdge* ce = b->contact_list_; ce; ce = ce->next) {
+          Contact* contact = ce->contact;
+          
+          // Has this contact already been added to an island?
+          if (contact->flags_ & Contact::IslandFlag) {
+            continue;
+          }
+          
+          // Is this contact solid and touching?
+          if (contact->IsEnabled() == false ||
+              contact->IsTouching() == false) {
+            continue;
+          }
+          
+          // Skip sensors.
+          bool sensorA = contact->fixture_a_->is_sensor_;
+          bool sensorB = contact->fixture_b_->is_sensor_;
+          if (sensorA || sensorB) {
+            continue;
+          }
+          
+          island.Add(contact);
+          contact->flags_ |= Contact::IslandFlag;
+          
+          Body* other = ce->other;
+          
+          // Was the other body already added to this island?
+          if (other->flags_ & Body::e_islandFlag) {
+            continue;
+          }
+          
+          IK_ASSERT(stackCount < stackSize);
+          stack[stackCount++] = other;
+          other->flags_ |= Body::e_islandFlag;
+        }
+        
+        // Search all joints connect to this body.
+        for (JointEdge* je = b->joint_list_; je; je = je->next) {
+          if (je->joint->is_land_flag_ == true) {
+            continue;
+          }
+          
+          Body* other = je->other;
+          
+          // Don't simulate joints connected to disabled bodies.
+          if (other->IsEnabled() == false) {
+            continue;
+          }
+          
+          island.Add(je->joint);
+          je->joint->is_land_flag_ = true;
+          
+          if (other->flags_ & Body::e_islandFlag) {
+            continue;
+          }
+          
+          IK_ASSERT(stackCount < stackSize);
+          stack[stackCount++] = other;
+          other->flags_ |= Body::e_islandFlag;
+        }
+      }
+      
+      Profile profile;
+      island.Solve(&profile, step, gravity_, allow_sleep_);
+      profile.solveInit += profile.solveInit;
+      profile.solveVelocity += profile.solveVelocity;
+      profile.solvePosition += profile.solvePosition;
+      
+      // Post solve cleanup.
+      for (int32_t i = 0; i < island.body_count_; ++i) {
+        // Allow static bodies to participate in other islands.
+        Body* b = island.bodies_[i];
+        if (b->GetType() == BodyType::StaticBody) {
+          b->flags_ &= ~Body::e_islandFlag;
+        }
+      }
+    }
   }
   
   bool World::IsLocked() const {
