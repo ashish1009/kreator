6607061c84c98a32f38df7ac496f0552a631794c
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-01-19 12:59:46 +0530

    Physics : Updating Setting File for allocation and free up the memory

--- ikan/interface/physics/utils/settings.hpp
+++ ikan/interface/physics/utils/settings.hpp
@@ -11,12 +11,22 @@ namespace physics {
   
   namespace utils {
     
+    // Default allocation and free API
+    void* AllocImpl(int32_t size);
+    void FreeImpl(void* mem);
+
     /// This function allocate the memory of size
     /// - Parameter size: size of memory allocation
-    void* Alloc(int32_t size);
+    /// - Important: Change Implementation for User Defined Allocation
+    void* Alloc(int32_t size) {
+      return AllocImpl(size);
+    }
     /// This function delete the allocated memory
     /// - Parameter mem: memory pointer to be deleted
-    void Free(void* mem);
+    /// - Important: Change Implementation for User Defined Free
+    void Free(void* mem) {
+      FreeImpl(mem);
+    }
     
   }
   
