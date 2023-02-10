8666bedce7b6848adbab2176b8771e8e721ca9cd
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-02-11 01:34:25 +0530

    Adding Player entity in player class

--- kreator/layers/mario/new_game/player.cpp
+++ kreator/layers/mario/new_game/player.cpp
@@ -11,8 +11,15 @@ namespace mario {
   
   void Player::Init(std::shared_ptr<EnttScene> scene) {
     scene_ = scene;
+    entity_ = scene_->CreateEntity("Player");
+    auto& qc = entity_.AddComponent<QuadComponent>();
+
+    qc.texture_comp.use = true;
+    qc.texture_comp.use_sprite = true;
+    qc.texture_comp.linear_edge = false;
     
-//    entity = scene_->CreateEntity("Player");
+    qc.texture_comp.component = Renderer::GetTexture(AM::ClientAsset("textures/player.png"), qc.texture_comp.linear_edge);
+    qc.texture_comp.sprite = SubTexture::CreateFromCoords(qc.texture_comp.component, {6.0f, 30.0f});
   }
   
 }
