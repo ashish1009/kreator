e9bbcaac52272f616f90e7e96faffcef8c9deec9
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-03-10 17:39:59 +0530

    Check player on Ground or not

--- kreator/layers/ikan_game_editor/games/mario/src/player.cpp
+++ kreator/layers/ikan_game_editor/games/mario/src/player.cpp
@@ -18,6 +18,35 @@ namespace mario {
   PlayerController* PlayerController::Get() {
     return instance_;
   }
+
+  void PlayerController::Create(Entity entity)  {
+    entity_ = entity;
+    
+    if (player_state_ == PlayerState::Small) {
+      player_width_ = 1.0f;
+      player_height_ = 1.0f;
+    }
+    else if (player_state_ == PlayerState::Big) {
+      player_width_ = 1.0f;
+      player_height_ = 2.0f;
+    }
+  }
+  
+  void PlayerController::Update(Timestep ts) {
+    CheckOnGround();
+  }
+  
+  void PlayerController::CheckOnGround() {
+    float inner_player_width = player_width_ * 0.6f;
+    float y_val = -(player_height_ / 2);
+    y_val -= (player_state_ == PlayerState::Small) ? 0.02f : 0.02f;
+    
+    on_ground_ = entity_.GetScene()->CheckOnGround(&entity_, inner_player_width, y_val);
+  }
+
+  void PlayerController::RenderGui() {
+    ImGui::Text(" On Ground     | %s", on_ground_ ? "True" : "False");
+  }
   
 }
  
--- kreator/layers/ikan_game_editor/games/mario/src/player.hpp
+++ kreator/layers/ikan_game_editor/games/mario/src/player.hpp
@@ -11,13 +11,30 @@ namespace mario {
   
   using namespace ikan;
 
+  enum class PlayerState {
+    Small, Big, Fire, Invicible, Luigi
+  };
+
   class PlayerController : public ScriptableEntity {
   public:
     PlayerController();
-    
+  
+    void Create(Entity entity) override;
+    void Update(Timestep ts) override;
+    void RenderGui() override;
+
     static PlayerController* Get();
     
   private:
+    void CheckOnGround();
+    
+    bool reset_fixture_ = false;
+    bool on_ground_ = false;
+    
+    float player_width_ = 1.0f, player_height_ = 1.0f;;
+
+    PlayerState player_state_ = PlayerState::Small;
+
     static PlayerController* instance_;
   };
   
